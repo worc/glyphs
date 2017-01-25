60cb43a5f07063046b2138bb9d44d93df7eac2c2
worc <curtis.s.mcallister@gmail.com>
2017-01-24 21:49:06 -0800

    add toSvg stub, add toCanvas and toBase64 functions, add dependency on GlyphCanvas

--- ingress-glyph-tools/Glyph.js
+++ ingress-glyph-tools/Glyph.js
@@ -1,6 +1,23 @@
+var GlyphCanvas = require('./GlyphCanvas');
+
 function Glyph(name, edges) {
     this.name = name;
     this.edges = edges;
 }
+
+Glyph.prototype.toSvg = function() {
+    console.log('todo');
+};
+
+Glyph.prototype.toCanvas = function(height) {
+    this.canvas = new GlyphCanvas(height, this.edges);
+    return this.canvas;
+};
+
+Glyph.prototype.toBase64 = function(height, webHeader) {
+    return (webHeader) ?
+        this.toCanvas(height).toDataURL():
+        this.toCanvas(height).toDataURL().replace('data:image/png;base64,', '');
+};
 
 module.exports = Glyph;
