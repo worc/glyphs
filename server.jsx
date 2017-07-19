6fb3a35b43ef83f30650be8d0f6863420c1731f3
worc <curtis.s.mcallister@gmail.com>
2017-07-18 19:02:14 -0700

    remove express route for individual glyphs and glyph sequences

--- server.jsx
+++ server.jsx
@@ -2,10 +2,11 @@ import express from "express";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import React from "react";
+import path from "path";
 
-// import App from "./shared/App";
-import GlyphSequence from "./shared/components/GlyphSequence";
-import Glyphtionary from "./shared/components/Glyphtionary";
+import App from "./shared/App";
+// import GlyphSequence from "./shared/components/GlyphSequence";
+// import Glyphtionary from "./shared/components/Glyphtionary";
 
 const app = express();
 
@@ -24,6 +25,7 @@ const renderPage = (title, app) => `
         <div id="app">${renderToString(app)}</div>
         
       </body>
+      <script src="/static/client.js"></script>
     </html>
   </html>
 `;
@@ -32,23 +34,25 @@ app.get("/", (req, res) => {
   res.redirect("/glyphs");
 });
 
-app.get("/glyphs", (req, res) => {
+app.get("/glyphs/:glyphs?", (req, res) => {
   let pageTitle = "Ingress Glyphtionary";
 
   res.status(200).send(renderPage(pageTitle, (
     <StaticRouter context={{}} location={req.url}>
-      <Glyphtionary />
+      <App />
     </StaticRouter>
   )));
 });
 
-app.get("/glyphs/:glyph", (req, res) => {
-  let delimiters = new RegExp(/[+\-_]/); // allow for + - and _ to be delimiters for glyph sequences
-  let glyphSequence = req.params.glyph.split(delimiters);
+// app.get("/glyphs/:glyph", (req, res) => {
+//   let delimiters = new RegExp(/[+\-_]/); // allow for + - and _ to be delimiters for glyph sequences
+//   let glyphSequence = req.params.glyph.split(delimiters);
+//
+//   res.status(200).send(renderPage(req.params.glyph, (
+//     <GlyphSequence names={glyphSequence} />
+//   )));
+// });
 
-  res.status(200).send(renderPage(req.params.glyph, (
-    <GlyphSequence names={glyphSequence} />
-  )));
-});
+app.use("/static/client.js", express.static(path.join(__dirname, "dist/client.js")));
 
 export default app;
