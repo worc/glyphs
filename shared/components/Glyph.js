8d1ecfa0db59d761c46c1e956894a9ff51daf42b
worc <curtis.s.mcallister@gmail.com>
2017-08-01 13:26:18 -0700

    add state for color and line width styles, add state change handlers, add GlyphStyle to render, change from an image to a GlyphCanvas in the render

--- shared/components/Glyph.js
+++ shared/components/Glyph.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Glyphs from "../../ingress-glyph-tools/Glyphs";
-import GlyphCanvas from "../../ingress-glyph-tools/GlyphCanvas";
+import Glyphs from "../utils/Glyphs";
+import GlyphCanvas from "./GlyphCanvas";
+import GlyphStyle from "./GlyphStyle";
 
 class Glyph extends React.Component {
 
@@ -10,20 +11,77 @@ class Glyph extends React.Component {
     this.state = {
       glyph : Glyphs.find((element) => {
         return element.name.indexOf(this.props.name) > -1;
-      })
-    }
+      }),
+
+      // override default settings sent down by glyph sequence
+      style: {
+        height: (this.props.sequenceHeight) ? this.props.sequenceHeight : 100,
+        width: (this.props.sequenceWidth) ? this.props.sequenceHeight : 100,
+
+        trace: {
+          red: 255,
+          green: 155,
+          blue: 155,
+          alpha: 0.8,
+          lineWidth: 5
+        },
+        nodes: {
+          red: 0,
+          green: 0,
+          blue: 0,
+          alpha: 1,
+          radius: 3
+        },
+        hexagon: {
+          red: 0,
+          green: 0,
+          blue: 0,
+          alpha: 1,
+          lineWidth: 1
+        }
+      }
+    };
+
+    this.onSizeChange = this.onSizeChange.bind(this);
+    this.onColorChange = this.onColorChange.bind(this);
+  }
+
+  onSizeChange(e) {
+    let size = e.target.value;
+
+    this.setState(prevState => {
+      let newState = Object.assign({}, prevState);
+
+      newState.style.height = size;
+      newState.style.width = size;
+
+      return newState;
+    });
+  }
+
+  onColorChange(e) {
+    let name = e.currentTarget.name;
+    let input = e.target.name;
+    let value = e.target.value;
+
+    this.setState(prevState => {
+      let newState = Object.assign({}, prevState);
+
+      newState.style[name][input] = value;
+
+      return newState;
+    });
   }
 
   render() {
-    let canvas = new GlyphCanvas(this.props.height, this.state.glyph.edges);
-
     return (
-      <div style={this.props.style} >
-        <img src={ canvas.toDataURL() } />
+      <div style={this.props.glyphDisplayStyle} >
+        <GlyphCanvas style={this.state.style} height={this.state.style.height} width={this.state.style.width} edges={this.state.glyph.edges} />
         <div>{this.state.glyph.name.join(", ")}</div>
+        <GlyphStyle style={this.state.style} height={this.state.style.height} width={this.state.style.width} onSizeChange={this.onSizeChange} onColorChange={this.onColorChange}/>
       </div>
     )
   }
 }
 
-module.exports = Glyph;
+export default Glyph;
