559e4d8dfbfa09f0551d73ea549ddbb3fe2271d4
worc <curtis.s.mcallister@gmail.com>
2017-08-03 16:14:12 -0700

    add a second webpack entry for server side bundlings, comment out externals option (i don't think it's actually doing anything)

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,59 @@
 const path = require("path");
 
-module.exports = {
-  entry: path.resolve(__dirname, "client/index.js"),
+module.exports = [
+  {
+    name: "browser",
+    entry: path.resolve(__dirname, "client/index.js"),
+    target: "web", // fixes "cannot resolve 'fs'" error
 
-  target: "web", // fixes "cannot resolve 'fs'" error
+    output: {
+      path: path.resolve(__dirname),
+      filename: "dist/client.js"
+    },
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "client.js"
-  },
+    resolve: {
+      modules: ["client", "shared", "node_modules"]
+    },
 
-  resolve: {
-    modules: ["client", "shared", "node_modules"]
-  },
+    // externals: {
+    //   fs: "fs",
+    //   canvas: "canvas"
+    // },
 
-  externals: {
-    fs: "fs",
-    canvas: "canvas"
-  },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader"
+        }
+      ]
+    },
+  }, {
+    name: "server",
+    entry: "./server.jsx",
+    target: "node",
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader"
-      }
-    ]
-  },
-};
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "server.js"
+    },
+
+    resolve: {
+      modules: ["client", "shared", "node_modules"]
+    },
+
+    // externals: {
+    //   fs: "fs",
+    //   canvas: "canvas"
+    // },
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+        }
+      ]
+    },
+  }
+];
