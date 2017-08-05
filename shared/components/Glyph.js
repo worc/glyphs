e1db74c1e14b0e055bd0cc7f75f90036a375db17
worc <curtis.s.mcallister@gmail.com>
2017-08-05 14:18:11 -0700

    comment out change handlers and local state for the Glyph component
    at some point i would like to have another setting where individual glyphs can override the sequence styling, but for now i just want to get chroma-js working. with colors easily converted between chroma objects and css strings, it'll be easier to generate glyph styles from the url query parameters. and query parameters for each individual glyph in a sequence is a bit much right now

--- shared/components/Glyph.js
+++ shared/components/Glyph.js
@@ -14,71 +14,70 @@ class Glyph extends React.Component {
       }),
 
       // override default settings sent down by glyph sequence
-      style: {
-        height: (this.props.sequenceHeight) ? this.props.sequenceHeight : 100,
-        width: (this.props.sequenceWidth) ? this.props.sequenceHeight : 100,
-
-        trace: {
-          red: 255,
-          green: 155,
-          blue: 155,
-          alpha: 0.8,
-          lineWidth: 5
-        },
-        nodes: {
-          red: 0,
-          green: 0,
-          blue: 0,
-          alpha: 1,
-          radius: 3
-        },
-        hexagon: {
-          red: 0,
-          green: 0,
-          blue: 0,
-          alpha: 1,
-          lineWidth: 1
-        }
-      }
+      // style: {
+      //   height: (this.props.sequenceHeight) ? this.props.sequenceHeight : 100,
+      //   width: (this.props.sequenceWidth) ? this.props.sequenceHeight : 100,
+      //
+      //   trace: {
+      //     red: (this.props.style.traceColor.rgba()[0] || 255),
+      //     green: 155,
+      //     blue: 155,
+      //     alpha: 0.8,
+      //     lineWidth: 5
+      //   },
+      //   nodes: {
+      //     red: 0,
+      //     green: 0,
+      //     blue: 0,
+      //     alpha: 1,
+      //     radius: 3
+      //   },
+      //   hexagon: {
+      //     red: 0,
+      //     green: 0,
+      //     blue: 0,
+      //     alpha: 1,
+      //     lineWidth: 1
+      //   }
+      // }
     };
 
-    this.onSizeChange = this.onSizeChange.bind(this);
-    this.onColorChange = this.onColorChange.bind(this);
+    // this.onSizeChange = this.onSizeChange.bind(this);
+    // this.onColorChange = this.onColorChange.bind(this);
   }
 
-  onSizeChange(e) {
-    let size = e.target.value;
-
-    this.setState(prevState => {
-      let newState = Object.assign({}, prevState);
-
-      newState.style.height = size;
-      newState.style.width = size;
-
-      return newState;
-    });
-  }
-
-  onColorChange(e) {
-    let name = e.currentTarget.name;
-    let input = e.target.name;
-    let value = e.target.value;
-
-    this.setState(prevState => {
-      let newState = Object.assign({}, prevState);
-
-      newState.style[name][input] = value;
-
-      return newState;
-    });
-  }
+  // onSizeChange(e) {
+  //   let size = e.target.value;
+  //
+  //   this.setState(prevState => {
+  //     let newState = Object.assign({}, prevState);
+  //
+  //     newState.style.height = size;
+  //     newState.style.width = size;
+  //
+  //     return newState;
+  //   });
+  // }
+  //
+  // onColorChange(e) {
+  //   let name = e.currentTarget.name;
+  //   let input = e.target.name;
+  //   let value = e.target.value;
+  //
+  //   this.setState(prevState => {
+  //     let newState = Object.assign({}, prevState);
+  //
+  //     newState.style[name][input] = value;
+  //
+  //     return newState;
+  //   });
+  // }
 
   render() {
     return (
       <div style={this.props.glyphDisplayStyle} >
-        <GlyphCanvas style={this.state.style} height={this.state.style.height} width={this.state.style.width} edges={this.state.glyph.edges} />
+        <GlyphCanvas style={this.props.style} height={this.props.style.height} width={this.props.style.width} edges={this.state.glyph.edges} />
         <div>{this.state.glyph.name.join(", ")}</div>
-        <GlyphStyle style={this.state.style} height={this.state.style.height} width={this.state.style.width} onSizeChange={this.onSizeChange} onColorChange={this.onColorChange}/>
       </div>
     )
   }
